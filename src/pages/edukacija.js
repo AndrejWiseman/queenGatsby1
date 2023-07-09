b4e7b03d6dfe2b7f1769eb3926d02e63ff382cce
A.M <andrej2000aa@gmail.com>
2023-07-09 10:05:06 +0200

    prvi push sa webstorm 2

---
+++ src/pages/edukacija.js
@@ -0,0 +1,25 @@
+import * as React from "react"
+import Layout from "../components/Layout"
+import "../assets/css/main.css"
+import SEO from "../components/SEO";
+
+
+
+const Edukacije = () => {
+  return (
+      <>
+        <Layout>
+          <SEO title="Edukacije" />
+          <section className="section1">
+            <h2 className="naslovPriprema">EDUKACIJE</h2>
+            <div className="priprema">
+              <h3>Stranica je u pripremi</h3>
+            </div>
+          </section>
+
+        </Layout>
+      </>
+  )
+}
+
+export default Edukacije
